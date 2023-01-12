613b89d54ffa66e44969a59170a85b1e6ee3999a
Gaye Y <96841321+gaiaysl@users.noreply.github.com>
2023-01-12 21:30:32 +0300

    backgroundcolor fixed

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -36,9 +36,9 @@ value={category}
 onChange={(e) =>
     setCategory(e.target.value)}
  id="countries" 
- className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+ className="  bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
 {categories.map((option)=> (
-  <option>{option.value}</option>
+  <option >{option.value}</option>
 ))}
 
 </select>
